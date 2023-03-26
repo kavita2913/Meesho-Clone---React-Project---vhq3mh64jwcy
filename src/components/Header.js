01208a75564c8e8837d49694e2866d893c58b3c5
kavita2913 <kavita299910@gmail.com>
2023-03-26 13:01:15 +0530

    cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,6 @@ function Header() {
         <button className='cartButtton'>
           <i className="fa fa-shopping-cart cartIcon"></i>
           <Link className='cartpara' to="/cartdisplay">Cart{cartData.dataApp.length}</Link></button>
-        <button className='logoutBtn' onClick={logoutBtn}>Logout</button>
       </nav>
     </div>
   )
